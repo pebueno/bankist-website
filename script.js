dccabbd2d2c2374b6dab039f46daef97eeebf4fd
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2023-04-05 13:03:26 -0300

    Implement smooth scroll

--- script.js
+++ script.js
@@ -29,6 +29,23 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // })
+
+  section1.scrollIntoView({behavior: 'smooth'})
+});
+
 /*
 
 ///////////////////////////////////////
