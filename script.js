142c159d8909afceb527a66dce8cadf5d439b3b2
Pedro Ivo Bueno Sartorio <pedroivobu@gmail.com>
2023-04-05 19:48:53 -0300

    Implement Reveal sections

--- script.js
+++ script.js
@@ -147,7 +147,7 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // observer.observe(section1);
 
 const header = document.querySelector('.header');
-const navHeight = nav.getBoundingClientRect().height
+const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -163,6 +163,32 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+
 /*
 
 ///////////////////////////////////////
